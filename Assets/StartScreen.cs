c5eb293b0ea5f78b7ef300309c7445df8436d597
boominh <michynuggy@gmail.com>
2023-10-06 23:20:01 +0200

    started start screen

--- Assets/StartScreen.cs
+++ Assets/StartScreen.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class StartScreen : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // slider fill percentage
+    // draw own picture
+    // spawn acorn
+    // template
+    // camera
+
     void Start()
     {
-        Time.timeScale = 0;
+
     }
 
-    // Update is called once per frame
     void Update()
     {
         
